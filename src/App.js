512682d80c62a15765e4fb2f8dd5cca2224546cc
SANDRA-S-MENON <sandramenon2002@gmail.com>
2024-06-13 23:56:00 +0530

    signup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import logo from './logo.svg';
 import './App.css';
 import AddBus from './components/AddBus';
+import SignUp from './components/SignUp';
 
 function App() {
   return (
     <div>
+      <SignUp/>
       <AddBus/>
     </div>
   );
